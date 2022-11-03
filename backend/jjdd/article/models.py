573cdd7e6c0f6e61d6a3e21a0716c9a1370f0807
Moshed-Potato <rudah03@naver.com>
2022-11-03 21:02:50 +0900

    ➕Article model setting

--- backend/jjdd/article/models.py
+++ backend/jjdd/article/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+  title = models.TextField()
+  datetime = models.DateTimeField()
+  preview_prologue = models.TextField()
+  detail_link_postfix = models.TextField()
+  preview_img_path = models.TextField()
+  detail_img_path = models.TextField()
+  journal_name = models.TextField()
+  detail_text = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
