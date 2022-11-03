f80d18283452f4f1ff5b9f0bc0c693f05ed37403
Moshed-Potato <rudah03@naver.com>
2022-11-03 21:37:34 +0900

    article GET method

--- backend/jjdd/article/views.py
+++ backend/jjdd/article/views.py
@@ -1,10 +1,32 @@
 from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
 from django.shortcuts import render
 from .models import Article
+import json
 
 
 def article(request):
-  pass
+  if request.method == "GET":
+    article_list = [{ 'title': article['title'],
+                     'datetime': article['datetime'],
+                     'preview_prologue': article['preview_prologue'],
+                     'detail_link_postfix': article['detail_link_postfix'],
+                     'preview_img_path': article['preview_img_path'],
+                     'detail_img_path': article['detail_img_path'],
+                     'journal_name': article['journal_name'],
+                     'detail_text': article['detail_text'],
+                     'created_at': article['created_at'],
+                     'updated_at': article['updated_at'],
+                     }
+                    for article in Article.objects.all().values()]
+    return JsonResponse(article_list, safe=False)
+      
+  elif request.method == "POST":
+    req_data = json.loads()
+    
+  else:
+    return HttpResponseNotAllowed(["GET", "POST"])
+  
+  
 
 def article_detail(request, article_id):
   pass
