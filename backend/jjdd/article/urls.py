d69e77bcd5c5386f3cc65a25046aa30d19d5b31b
Moshed-Potato <rudah03@naver.com>
2022-11-03 20:50:56 +0900

    urls.py initial setting

--- backend/jjdd/article/urls.py
+++ backend/jjdd/article/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from article import views
 
 urlpatterns = [
-    # path('token/', views.token, name='token'), -> user로 뺄 예정
     path('article/', views.articles, name='articles'),
     path('article/<int:article_id>/', views.article_detail, name='article_detail'),
 ]
--- backend/jjdd/user/views.py
+++ backend/jjdd/user/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
