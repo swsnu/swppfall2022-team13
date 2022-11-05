21b9a2b2e33d0fa6c0366cc3c724c8bb069c570c
Toa <ld980105@snu.ac.kr>
2022-11-05 13:58:35 +0900

    add politicianReducer

--- frontend/src/containers/PoliticianDetailPage/PoliticianDetailPage.tsx
+++ frontend/src/containers/PoliticianDetailPage/PoliticianDetailPage.tsx
@@ -1,14 +1,15 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, Navigate } from "react-router-dom";
+import { useParams } from "react-router";
+
 const PoliticianDetailPage = () => {
-    return (
-        <>
-          <h1>This is JJDD PoliticianDetailPage</h1>
-  
-              <p>
-              so what
-              </p>
-  
-        </>
-      );
-  }
-  
-  export default PoliticianDetailPage
+  return (
+    <div>
+      <h1>This is JJDD PoliticianDetailPage</h1>
+      <p>so what</p>
+    </div>
+  );
+};
+
+export default PoliticianDetailPage;
---
+++ frontend/src/store/index.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import articleReducer from "./slices/article";
+import politicianReducer from "./slices/politician";
+
+export const store = configureStore({
+  reducer: {
+    article: articleReducer,
+    politician: politicianReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
