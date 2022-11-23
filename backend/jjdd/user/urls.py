c99e3cdd7afc26f16c73d35065c9975280c31e66
Moshed-Potato <rudah03@naver.com>
2022-11-23 18:00:23 +0900

    user setting

--- backend/jjdd/user/urls.py
+++ backend/jjdd/user/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from user import views
 
 urlpatterns = [
-    path('token/', views.token, name='token'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     path('signout/', views.signout, name='signout'),
--- backend/jjdd/user/views.py
+++ backend/jjdd/user/views.py
@@ -1,6 +1,48 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+from django.contrib.auth.models import User
+from django.shortcuts import HttpResponsePermanentRedirect
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.contrib.auth import authenticate, login, logout
+from .models import Article, Comment
 import json
 
-# Create your views here.
+@ensure_csrf_cookie
+def token(request):
+    if request.method == 'GET':
+        return HttpResponse(status=204)
+    else:
+        return HttpResponseNotAllowed(['GET'])
+
+def signup(request):
+    if request.method == 'POST':
+        req_data = json.loads(request.body.decode())
+        username = req_data['username']
+        # print(username);
+        password = req_data['password']
+        User.objects.create_user(username=username, password=password)
+        return HttpResponse(status=201)
+    else:
+        return HttpResponseNotAllowed(['POST'])
+      
+def signin(request):
+    if request.method == 'POST':
+        req_data = json.loads(request.body.decode())
+        username = req_data['username']
+        password = req_data['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user != None:
+            login(request, user)
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=401)
+    
+    else:
+        return HttpResponseNotAllowed(['POST'])
+  
+def signout(request):
+    if request.method == 'GET':
+        logout(request)
+        return HttpResponse(status=200)
+    else:
+        return HttpResponseNotAllowed(['GET'])
