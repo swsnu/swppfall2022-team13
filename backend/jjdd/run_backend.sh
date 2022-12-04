77e1808fb3f91ea4a0454c8cc99ece7ac17f47f3
Moshed-Potato <rudah03@naver.com>
2022-12-04 23:26:33 +0900

    edit comment for learning

--- backend/jjdd/run_backend.sh
+++ backend/jjdd/run_backend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# TODO: Write automation script for launching BE app
+# 컨테이너 켜질 때마다 아래의 커맨드들을 실행시킨다.
 python manage.py makemigrations 
 python manage.py migrate
 mkdir -p /log # for `uwsgi` logging 
