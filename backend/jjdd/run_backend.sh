2ad1ffbc5d7da416e41a1fd002dee6c03daa809e
Moshed-Potato <rudah03@naver.com>
2022-12-04 23:39:35 +0900

    reset

--- backend/jjdd/run_backend.sh
+++ backend/jjdd/run_backend.sh
@@ -3,5 +3,5 @@
 # 컨테이너 켜질 때마다 아래의 커맨드들을 실행시킨다.
 python manage.py makemigrations 
 python manage.py migrate
-sudo mkdir -p /log # for `uwsgi` logging 
+mkdir -p /log # for `uwsgi` logging 
 uwsgi --ini uwsgi/uwsgi.ini
