63b402140b511fee01336d06ff73c6cf53ea8b0c
rickyini3028 <47096273+rickyini3028@users.noreply.github.com>
2022-12-06 14:46:44 +0900

    comment display

---
+++ frontend/src/components/Comment/Comment.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "../../store";
+import { selectUser} from "../../store/slices/user";
+import { deleteComment } from "../../store/slices/comment";
+import axios from 'axios';
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+export interface IProps {
+    id: number
+    quora_id: number
+    author_id: number
+    content: string
+    clickDetail?: () => void; // Defined by React
+    clickDelete?: () => void;
+  }
+  
+  const Comment = (props: IProps) => {
+
+    const dispatch = useDispatch<AppDispatch>();
+    const userState = useSelector(selectUser);
+    const [commentDisplay, setCommentDisplay] = useState(props.content);
+    const [user_email, setUserEmail] = useState("");
+    const [user_id, setUserId] = useState(0);
+
+    
+    
+    useEffect(() => {
+        async function fetchAndSetUser() { 1
+            const response = await axios.get("/api/user/islogin/");
+            console.log("Login status: ", response.data);
+            const isLogin = response['data']['status'];
+
+            if(isLogin && response.data.id !== 2){
+            setUserEmail(response['data']['email']);
+            setUserId(response['data']['id']);
+            }
+
+        }
+        fetchAndSetUser();
+      }, []);
+
+    const clickDelete = () => {       
+        dispatch(deleteComment(props.id));
+      }  
+
+
+    if(user_id === props.author_id) {
+      return (
+        <div className="CommentsEach">
+          <div className="commentorName" id="comment-author">{user_email}</div>
+          <b><div className="commentContent" id="comment-content">{commentDisplay}</div></b>
+          <p></p>
+          <button type="button" id="delete-comment-button" onClick={clickDelete}>delete comment</button>
+          <p></p>
+  
+        </div>
+      );
+    } else {
+      return (
+        <div className="CommentsEach">
+          <div className="commentorName" id="comment-author">{user_email}</div>
+          <b><div className="commentContent" id="comment-content">{commentDisplay}</div></b>
+          <p></p>
+        </div>
+      );
+    }
+  };
+  export default Comment;
