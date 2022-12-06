9ff1ae2e4afac11b526ff938ff3fa3b5a8ecec13
rickyini3028 <47096273+rickyini3028@users.noreply.github.com>
2022-12-06 14:04:13 +0900

    test fixed

--- frontend/src/containers/NewsListPage/NewsListPage.test.tsx
+++ frontend/src/containers/NewsListPage/NewsListPage.test.tsx
@@ -11,6 +11,7 @@ import NewsListPage from "./NewsListPage";
 import NewsArticle, {
   NewsArticleType,
 } from "../../components/NewsArticle/NewsArticle";
+import {stubInitialState, stubInitialState2, stubInitialState3, stubInitialState4, stubInitialState5 } from "../../test-utils/initialState";
 
 const mockNavigate = jest.fn();
 jest.mock("react-router", () => ({
@@ -63,92 +64,6 @@ jest.mock(
     )
 );
 
-const stubInitialState: ArticleState = {
-  articles: [
-    {
-      id: 1,
-      title: "ang gimochi",
-      content: "ang gimochi",
-      datetime_str: "ang gimochi",
-      preview_prologue: "ang gimochi",
-      detail_link_postfix: "ang gimochi",
-      preview_img_path: "ang gimochi",
-      detail_img_path: "ang gimochi",
-      journal_name: "한겨레",
-      detail_text: "ang gimochi",
-      created_at: "ang gimochi",
-      updated_at: "ang gimochi",
-    },
-    {
-      id: 2,
-      title: "ang gimochi",
-      content: "ang gimochi",
-      datetime_str: "ang gimochi",
-      preview_prologue: "ang gimochi",
-      detail_link_postfix: "ang gimochi",
-      preview_img_path: "ang gimochi",
-      detail_img_path: "ang gimochi",
-      journal_name: "조선일보",
-      detail_text: "ang gimochi",
-      created_at: "ang gimochi",
-      updated_at: "ang gimochi",
-    },
-  ],
-  selectedArticle: null,
-};
-
-const stubInitialState2: PoliticianState = {
-  politicians: [
-    {
-      id: 1,
-      name: "ang gimochi",
-      birth_date: "ang gimochi",
-      job: "ang gimochi",
-      image_src: "ang gimochi",
-      political_party: "ang gimochi",
-      election_precinct: "ang gimochi",
-      committee: "ang gimochi",
-      committees: "ang gimochi",
-      reelection: "ang gimochi",
-      election_units: "ang gimochi",
-      email: "ang gimochi",
-      career_summary: "ang gimochi",
-      mona_code: "ang gimochi",
-      proposals: "test",
-    },
-  ],
-  selectedPolitician: null,
-};
-const stubInitialState3: PetitionState = {
-  petitions: [
-    {
-      id: 1,
-      title: "ang gimochi",
-      content: "ang gimochi",
-      author: 1,
-      vote: 1,
-      photo_url: "test",
-    },
-  ],
-  selectedPetition: null,
-};
-
-const stubInitialState4: QuoraState = {
-  quoras: [
-    {
-      id: 1,
-      title: "ang gimochi",
-      content: "ang gimochi",
-      author: 1,
-    },
-  ],
-  selectedQuora: null,
-};
-
-const stubInitialState5: UserType = {
-    email: "",
-    pw: ""
-}
 
 const mockStore = getMockStore({
   article: stubInitialState,
--- frontend/src/containers/QuoraListPage/QuoraListPage.test.tsx
+++ frontend/src/containers/QuoraListPage/QuoraListPage.test.tsx
@@ -11,6 +11,8 @@ import QuoraListPage from "./QuoraListPage";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Quora, { QuoraType } from "../../components/Quora/Quora";
+import {stubInitialState, stubInitialState2, stubInitialState3, stubInitialState4, stubInitialState5 } from "../../test-utils/initialState";
+
 
 const mockNavigate = jest.fn();
 jest.mock("react-router", () => ({
@@ -22,93 +24,6 @@ jest.mock("react-redux", () => ({
   ...jest.requireActual("react-redux"),
   useDispatch: () => mockDispatch,
 }));
-
-const stubInitialState: ArticleState = {
-  articles: [
-    {
-      id: 1,
-      title: "ang gimochi",
-      content: "ang gimochi",
-      datetime_str: "ang gimochi",
-      preview_prologue: "ang gimochi",
-      detail_link_postfix: "ang gimochi",
-      preview_img_path: "ang gimochi",
-      detail_img_path: "ang gimochi",
-      journal_name: "한겨레",
-      detail_text: "ang gimochi",
-      created_at: "ang gimochi",
-      updated_at: "ang gimochi",
-    },
-    {
-      id: 2,
-      title: "ang gimochi",
-      content: "ang gimochi",
-      datetime_str: "ang gimochi",
-      preview_prologue: "ang gimochi",
-      detail_link_postfix: "ang gimochi",
-      preview_img_path: "ang gimochi",
-      detail_img_path: "ang gimochi",
-      journal_name: "조선일보",
-      detail_text: "ang gimochi",
-      created_at: "ang gimochi",
-      updated_at: "ang gimochi",
-    },
-  ],
-  selectedArticle: null,
-};
-
-const stubInitialState2: PoliticianState = {
-  politicians: [
-    {
-      id: 1,
-      name: "ang gimochi",
-      birth_date: "ang gimochi",
-      job: "ang gimochi",
-      image_src: "ang gimochi",
-      political_party: "ang gimochi",
-      election_precinct: "ang gimochi",
-      committee: "ang gimochi",
-      committees: "ang gimochi",
-      reelection: "ang gimochi",
-      election_units: "ang gimochi",
-      email: "ang gimochi",
-      career_summary: "ang gimochi",
-      mona_code: "ang gimochi",
-      proposals: "test",
-    },
-  ],
-  selectedPolitician: null,
-};
-const stubInitialState3: PetitionState = {
-  petitions: [
-    {
-      id: 1,
-      title: "ang gimochi",
-      content: "ang gimochi",
-      author: 1,
-      vote: 1,
-      photo_url: "test",
-    },
-  ],
-  selectedPetition: null,
-};
-
-const stubInitialState4: QuoraState = {
-  quoras: [
-    {
-      id: 1,
-      title: "ang gimochi",
-      content: "ang gimochi",
-      author: 1,
-    },
-  ],
-  selectedQuora: null,
-};
-
-const stubInitialState5: UserType = {
-  email: "",
-  pw: ""
-}
 
 const mockStore = getMockStore({
 article: stubInitialState,
