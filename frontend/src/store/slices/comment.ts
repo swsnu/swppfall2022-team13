03fc5ad2e5e06d096a860308175f6f011f4cc6a3
rickyini3028 <47096273+rickyini3028@users.noreply.github.com>
2022-12-07 16:59:42 +0900

    quoras tested

--- frontend/src/store/slices/comment.ts
+++ frontend/src/store/slices/comment.ts
@@ -20,7 +20,8 @@ const initialState: CommentState = {
 };
 
 export const fetchComments = createAsyncThunk("comment/fetchComments", async () => {
-  const response = await axios.get<CommentType[]>("http://ec2-13-209-0-212.ap-northeast-2.compute.amazonaws.com:8000/api/comment/");
+  //const response = await axios.get<CommentType[]>("http://ec2-13-209-0-212.ap-northeast-2.compute.amazonaws.com:8000/api/comment/");
+  const response = await axios.get<CommentType[]>("/api/comment/");
   return response.data;
 });
 
@@ -35,7 +36,8 @@ export const fetchComment = createAsyncThunk(
 export const postComment = createAsyncThunk(
   "comment/postComment",
   async (td: Pick<CommentType, "quora_id" | "author_id" | "content">, { dispatch }) => {
-    const response = await axios.post("http://ec2-13-209-0-212.ap-northeast-2.compute.amazonaws.com:8000/api/comment/", td);
+    //const response = await axios.post("http://ec2-13-209-0-212.ap-northeast-2.compute.amazonaws.com:8000/api/comment/", td);
+    const response = await axios.post("/api/comment/", td);
     dispatch(commentActions.addComment(response.data));
   }
 );
@@ -43,7 +45,8 @@ export const postComment = createAsyncThunk(
 export const deleteComment = createAsyncThunk(
   "comment/deleteCOmment",
   async (id: CommentType["id"], { dispatch }) => {
-    await axios.delete(`http://ec2-13-209-0-212.ap-northeast-2.compute.amazonaws.com:8000/api/comment/${id}/`);
+    //await axios.delete(`http://ec2-13-209-0-212.ap-northeast-2.compute.amazonaws.com:8000/api/comment/${id}/`);
+    await axios.delete(`/api/comment/${id}/`);
     dispatch(commentActions.deleteComment({ targetId: id }));
   }
 );
