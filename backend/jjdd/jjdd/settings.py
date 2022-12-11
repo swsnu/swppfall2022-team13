eb74189f0488e1bcaaca4f0dc25da7376145a9f7
Ubuntu <ubuntu@ip-172-31-33-228.ap-northeast-2.compute.internal>
2022-12-11 14:59:44 +0000

    fix csrf issue

--- backend/jjdd/jjdd/settings.py
+++ backend/jjdd/jjdd/settings.py
@@ -73,18 +73,18 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://127.0.0.1:3000",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://jungjungdangdang.shop"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://127.0.0.1:3000",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://jungjungdangdang.shop"
 ]
 
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000'
-                         ,'http://localhost:3000']
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'jjdd.urls'
