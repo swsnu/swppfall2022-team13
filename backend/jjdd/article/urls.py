1b4669257d94024635a0c10ba062b37356437008
Moshed-Potato <rudah03@naver.com>
2022-12-13 22:31:17 +0900

    💿DB update for article like list

--- backend/jjdd/article/urls.py
+++ backend/jjdd/article/urls.py
@@ -4,6 +4,7 @@ from article import views
 urlpatterns = [
     path('', views.article, name='article'),
     path('<int:article_id>/', views.article_detail, name='article_detail'),
+    path('like/<int:user_id>/', views.like_articles, name='like_articles'),
     path('related/<int:article_id>/', views.related_articles, name='related_articles'),
     path('<str:politician_name>/', views.article_filter_by_politician, name='article_filter_by_politician')
 ]
